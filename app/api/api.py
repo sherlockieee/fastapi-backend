e7d8d6072e2db75b642819a8441fdbd563ebc402
sherlockieee <phuonghatrannguyen00@gmail.com>
2022-09-14 10:33:46 -0700

    remove all tags related

--- app/api/api.py
+++ app/api/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import projects, tags
+from app.api.endpoints import projects
 
 router = APIRouter()
 router.include_router(projects.router, prefix="/projects", tags=["projects"])
-router.include_router(tags.router, prefix="/tags", tags=["tags"])
+# router.include_router(tags.router, prefix="/tags", tags=["tags"])
 
 
 @router.get("/")
--- app/models/project_tags.py
+++ app/models/project_tags.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, ForeignKey, Table
-from sqlalchemy.orm import relationship
-from app.db import Base
+# from sqlalchemy import Column, ForeignKey, Table
+# from sqlalchemy.orm import relationship
+# from app.db import Base
 
 
-project_tags = Table(
-    "project_tags",
-    Base.metadata,
-    Column("project_id", ForeignKey("projects.id"), primary_key=True),
-    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
-)
+# project_tags = Table(
+#     "project_tags",
+#     Base.metadata,
+#     Column("project_id", ForeignKey("projects.id"), primary_key=True),
+#     Column("tag_id", ForeignKey("tags.id"), primary_key=True),
+# )
