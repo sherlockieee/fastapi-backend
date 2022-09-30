8ad9c3febcc3cef6c5ed533df171b9775b9f464e
sherlockieee <phuonghatrannguyen00@gmail.com>
2022-09-30 16:59:38 -0300

    fix token bug

--- app/utils/token.py
+++ app/utils/token.py
@@ -14,7 +14,7 @@ def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> 
         )
 
     to_encode = {"exp": expires_delta, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, settings.ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
     return encoded_jwt
 
 
